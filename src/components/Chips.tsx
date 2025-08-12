fd8b7f7dff3d06da25b52bb8af1dd382c45cfb74
Jonathan Maskew <jmaskew@purdue.edu>
2025-08-12 16:34:02 -0400

    optional color prop for chips

--- src/components/Chips.tsx
+++ src/components/Chips.tsx
@@ -1,14 +1,16 @@
 type ChipsProps = {
   strings: Array<string>;
+  color?: string;
 };
 
-export default function Chips({ strings }: ChipsProps) {
+export default function Chips({ strings, color }: ChipsProps) {
   return (
     <div className="flex flex-wrap gap-2">
       {strings.map((chipText, index) => (
         <div
           key={index}
-          className="px-3 py-1.5 bg-white/20 text-xs rounded-full text-nowrap"
+          className="px-3 py-1.5 text-xs rounded-full text-nowrap bg-white/20"
+          style={color ? { background: color } : undefined}
         >
           {chipText}
         </div>
