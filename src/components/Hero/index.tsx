7fd7e8ad9a0d75fe91d0c9559c197fc10263a9d7
Jonathan Maskew <jmaskew@purdue.edu>
2025-08-13 18:43:26 -0400

    slightly smaller images on mobile

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -8,26 +8,26 @@ import Image from 'next/image';
 export default function Hero() {
   return (
     <div className="flex flex-col gap-16 items-center p-6 h-full w-full justify-center">
-      <div className="relative w-full max-w-[325px] md:max-w-[500px] h-[350px] md:h-[200px] flex justify-center">
-        <div className="absolute transform rotate-6 left-0 top-45 md:top-5">
+      <div className="relative w-full max-w-[250px] md:max-w-[500px] h-[270px] md:h-[200px] flex justify-center">
+        <div className="absolute transform rotate-6 left-0 top-35 md:top-5">
           <Image
             src={turkeyRun}
             alt="Turkey Run hiking"
-            className="w-40 h-40 object-cover rounded-sm ring-6 border-white border-6"
+            className="w-30 h-30 md:w-40 md:h-40 object-cover rounded-sm ring-6 border-white border-6"
           />
         </div>
         <div className="absolute transform z-10">
           <Image
             src={graduation}
             alt="Costa Rica zip-lining"
-            className="w-50 h-50 object-cover rounded-sm ring-6 border-white border-6 shadow-2xl shadow-black"
+            className="w-40 h-40 md:w-50 md:h-50 object-cover rounded-sm ring-6 border-white border-6 shadow-2xl shadow-black"
           />
         </div>
-        <div className="absolute transform -rotate-6 right-0 top-45 md:top-5">
+        <div className="absolute transform -rotate-6 right-0 top-35 md:top-5">
           <Image
             src={costaRica}
             alt="Purdue graduation"
-            className="w-40 h-40 object-cover rounded-sm ring-6 border-white border-6"
+            className="w-30 h-30 md:w-40 md:h-40 object-cover rounded-sm ring-6 border-white border-6"
           />
         </div>
       </div>
